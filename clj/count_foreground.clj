96e26f9cdc6471ad7f4a3526d4a7d7f6a6ac1e15
Graeme Ball <graemeball@googlemail.com>
2013-11-02 19:22:03 +0000

    example clojure count_foreground script

---
+++ clj/count_foreground.clj
@@ -0,0 +1,10 @@
+(import '(ij WindowManager))
+(import '(ij IJ))
+(def imp (. WindowManager getCurrentImage))
+(. IJ run imp "Make Binary" "")
+(def nnonzero 
+  ((fn count-nonzero [imp] 
+    (count
+      (filter #(not (zero? %))
+        (. (. imp getProcessor) getPixels)))) imp))
+(. IJ log (str "number of foreground pixels: " nnonzero))
