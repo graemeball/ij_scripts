608706dfe37d2fd42aed0f59b737ca35399fef75
Graeme Ball <graemeball@googlemail.com>
2013-12-04 23:32:16 +0000

    added a couple of very simple imglib2 scripting examples (js, clj)

---
+++ clj/Imglib2_DisplayImg.clj
@@ -0,0 +1,11 @@
+; imglib2 scripting using clojure,
+; opens and shows an image
+
+(import ij.IJ)
+(import net.imglib2.img.Img)
+(import io.scif.img.ImgOpener)
+(import net.imglib2.img.display.imagej.ImageJFunctions)
+
+(def img (.openImg (new ImgOpener) "/Users/graemeb/Documents/testData/Lena.tif"))
+(def i (.iterator img))
+(. ImageJFunctions show img)
---
+++ js/Imglib2_DisplayView.js
@@ -0,0 +1,19 @@
+importClass(Packages.net.imglib2.RandomAccessibleInterval);
+importClass(Packages.net.imglib2.img.Img);
+importClass(Packages.io.scif.img.ImgOpener);
+importClass(Packages.net.imglib2.img.display.imagej.ImageJFunctions);
+importClass(Packages.net.imglib2.view.Views);
+
+// imglib2 scripting using javascript,
+// based on http://fiji.sc/ImgLib2_Examples Example 1d 
+
+// Open & show Lena image
+var img = new ImgOpener().openImg("/Users/graemeb/Documents/testData/Lena.tif");
+ImageJFunctions.show(img, "Lena");
+
+// Show a rotated view of the image
+ImageJFunctions.show(Views.rotate(img, 0, 1), "rotated Lena");
+
+// Create a View containing part of the image and display it
+var view = Views.interval(img, [80, 80], [180, 180]);
+ImageJFunctions.show(view, "Lena cropped");
