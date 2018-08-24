9f71d9de195d9671257977882da4507102f47242
Graeme Ball <graemeball@googlemail.com>
2018-08-24 17:06:21 +0100

    add jython script to normalise t-series by stdDev

---
+++ py/normalize_by_std.py
@@ -0,0 +1,28 @@
+# Normalize the intensity of each slice such that stdDev is constant over t
+# - normalize t-series for each channel and z-slice separately
+# - convert to 32-bit float for processing
+#
+# Copyright g.ball@dundee.ac.uk (2018) Dundee Imaging Facility
+# License: Creative Commons CC-BY-SA
+#
+
+from ij import IJ
+from ij import WindowManager
+
+imp = WindowManager.getCurrentImage()
+[w, h, nc, nz, nt] = imp.getDimensions()
+
+IJ.run("32-bit");  # to avoid rounding errors during intensity rescaling
+
+for c in range(nc):
+    for z in range(nz):
+        stdev0 = 1;
+        for t in range(nt):
+            imp.setPosition(c, z, t)
+            ip = imp.getProcessor()
+            stats = ip.getStats()
+            stdev = stats.stdDev
+            if t == 0:
+                stdev0 = stdev
+            scaleFactor = stdev0 / stdev;
+            IJ.run("Multiply...", "value=" + str(scaleFactor) + " slice")
